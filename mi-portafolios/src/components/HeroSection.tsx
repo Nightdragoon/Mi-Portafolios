ef3df7ecbddf065f474ef4760b9b7e3b58976f04
Nightdragoon <crackytrini@gmail.com>
2026-02-02 14:14:42 -0600

    mi avanze
    si mi avanze

---
+++ mi-portafolios/src/components/HeroSection.tsx
@@ -0,0 +1,18 @@
+
+
+function HeroSection() {
+
+  return (
+    <div className="container mx-auto">
+        <div className="flex justify-center align-center">
+            <h1>hola</h1>
+        </div>
+        <div className="flex justify-center align-center">
+            <h1>Como estas</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
